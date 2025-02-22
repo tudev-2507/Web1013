3be291db5af025355cd3836de312ce10c349cf78
tudev-2507 <vtu8531@gmail.com>
2025-02-22 08:08:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    thongbao.style.display = 'flex';
+}
+var span = document.getElementById("close");
+span.onclick = function () {
+    thongbao.style.display = 'none';
+}
